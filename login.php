e2096c58b6726bce3a355674ecae010247a6438a
Olufemi Oyemade <pharmtex08@yahoo.com>
2019-09-25 14:34:00 +0100

    Add files via upload

--- login.php
+++ login.php
@@ -1,58 +1,151 @@
 <?php
+
+
+
+include ('dbcon.php');
+if (isset($_POST['submit'])) {
+    // Initialize a session:
 session_start();
-if(isset($_POST['login-submit'])){
-    //user clicked submit button, implement logic
-require "./scripts/database.php";
-$username = $_POST['username'];
-$password = $_POST['password'];
+    $error = array();//this aaray will store all error messages
+  
 
-//Removing white space from user input
+    if (empty($_POST['email'])) {//if the email supplied is empty 
+        $error[] = 'You forgot to enter  your Email ';
+    } else {
 
-$username = trim($username);
-$password = trim($password);
 
-$_SESSION['loginError'] = array();
-if(empty($password) && empty($username)){
-    $_SESSION['loginError'] [] = "Fill in all fields". "</br>";
-    header('location: ./loginform.php');
-    exit();
-}
-else if(!preg_match("/^[a-zA-Z0-9]*$/", $username)){
-	$_SESSION['loginError'][] = "Username should contain only alphanumeric characters". "</br>";
-    header('location: ./loginform.php');
-    exit();
-}
-else if(empty($username)){
-	$_SESSION['loginError'][] = "Username is a required field". "</br>";
-    header('location: ../loginform.php');
-    exit();
-}
-else if(empty($password)){
-    $_SESSION['loginError'][] = "Password is a required field". "</br>";
-    header('location: ./loginform.php');
-    exit();
-}
-else{
+        if (preg_match("/^([a-zA-Z0-9])+([a-zA-Z0-9\._-])*@([a-zA-Z0-9_-])+([a-zA-Z0-9\._-]+)+$/", $_POST['email'])) {
+           
+            $email = $_POST['email'];// assigning user inut to variable 
+			$email = trim($email);// removing white space from the user input 
+        } 
+		else {
+             $error[] = 'Your EMail Address is invalid  ';
+        }
+
+
+    }
+
+
+    if (empty($_POST['password'])) {
+        $error[] = 'Please Enter Your Password ';
+    } else {
+        $password = $_POST['password'];// assigning user inut to variable 
+		$password = trim($password);// removing white space from the user input 
+			}
+
+
+       if (empty($error))//if the array is empty , it means no error found
+    { 
+
+       
+
+        $query_check_credentials = "SELECT * FROM users WHERE (Email='$email' AND Password='$password')";
+   
         
-    $sql = "SELECT * FROM users WHERE username='$username'";
+
+        $result_check_credentials = mysqli_query($dbc, $query_check_credentials);
+        if(!$result_check_credentials){//If the QUery Failed 
+            echo 'Query Failed ';
+        }
+
+        if (@mysqli_num_rows($result_check_credentials) == 1)//if Query is successfull 
+        { // A match was made.
+
+           
+
+
+            $_SESSION = mysqli_fetch_array($result_check_credentials, MYSQLI_ASSOC);//Assign the result of this query to SESSION Global Variable
+           
+            header("Location: budgetdb.php");
+          
+
+        }else
+        { 
+            
+            $msg_error= 'Either Your Account is inactive or Email address /Password is Incorrect';
+        }
+
+    }  else {
+        
+        
+
+echo '<div class="errormsgbox"> <ol>';
+        foreach ($error as $key => $values) {
+            
+            echo '	<li>'.$values.'</li>';
+
+
+       
+        }
+        echo '</ol></div>';
+
+    }
     
-    $result = $conn->query($sql);
     
-    $user = $result->fetch(PDO::FETCH_ASSOC);
-    
-	if($username !== $user['username'] || !password_verify($password, $user['password'])){
-        $_SESSION['loginError'][] = "Invalid login credentials. Please crosscheck your login details or click on the Sign Up link to create an Account";
-		header("location: login.php");
-		exit;
-    }elseif($username === $user['username'] && password_verify($password, $user['password'])){
-		header("location: ./success.php?loggedIn=");
-		exit;
-	}
-      
-}
-}
-else{
-//user did not click submit but got here through url modification redirect back to login page
-header('location: ./loginform.php');
-exit();
-}
+    if(isset($msg_error)){
+        
+        echo '<div class="warning">'.$msg_error.' </div>';
+    }
+    /// var_dump($error);
+    mysqli_close($dbc);
+
+} // End of the main Submit conditional.
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>BudgIt | login</title>
+    <link rel = "icon" href = "images\logo2.png" type = "image/png">
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css?family=Lobster|Lobster+Two&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+    <div class="container">
+        <div class="main">
+            <header>
+                <div id="logo-div">
+                    <img src="images/logo.png" id="logo" alt=""> <span id="logo-text">BudgIt</span> 
+                    <p>Making managing your finances hassle free</p>
+                </div>
+                <div id="nav-div" class="nav-div">
+                    <p> <a href="index.php"> Home</a>
+                        <a href=""> About</a>
+                        <a href="login.php" class="active" id="login"> Login</a>
+                        <a href="signup.php" id="signup"> Signup</a>
+                        <a href=""> Contact</a>
+                        <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+                                <img src="img/mdi_menu.png" alt="">
+                              </a>
+                    </p>
+                </div>
+            </header>
+            <div class="content">
+                <h1>Login to Manage Your Finances</h1>
+
+                <form id="form" action="login.php" method="POST">
+                    <i class="fa fa-user" style="color: #182955"></i><input type="text" name="email" id="email"  placeholder="Email" required><span id="Evalid"></span><br><br>
+                    <i class="fa fa-lock" style="color: #182955"></i><input type="password" name="password" id="password" placeholder="password"equired><br>
+                    <span class="right" style="color: #182955">Forgot Password?</span> <br><br>
+                    <button id="submit" type="submit" name="submit" value="Login">Login</button><br>
+                    <span>Don't have an account? no problem Signup <a href="signup.php">here</a>.</span>
+                </form>
+                
+               
+            </div>
+            <div class="clear"></div>
+            <footer>
+                <b>&copy;Copyright 2019 kymopoleia</b>
+            </footer>
+        </div>
+    </div>
+    <script src="script.js"></script>
+</body>
+</html>
--- signup.php
+++ signup.php
@@ -1,103 +1,186 @@
 <?php
-session_start();
-if(isset($_POST['signup-submit'])){
-//user clicked submit button, implement logic
-require('./scripts/database.php');
-// require "database.php";
-$firstname = $_POST['firstname'];
-$lastname = $_POST['lastname'];
-$email = $_POST['emailAddress'];
-$username = $_POST['username'];
-$password = $_POST['password'];
-$confirmPassword = $_POST['confirmPassword'];
-$_SESSION['errors'] = array();
-
-//Removing white space from user input
-
-$firstname = trim($firstname);
-$lastname = trim($lastname);
-$email = trim($email);
-$username = trim ($username);
-$password = trim ($password);
-$confirmPassword = trim($confirmPassword);
 
 
-if(empty($firstname) && empty($lastname) && empty($emailAddress) && empty($username) && empty($password) && empty($confirmPassword)){
-    $_SESSION['errors'] [] = "Fill in all fields". "</br>";
-    header('location: ./index.php');
-    exit();
-}
-else if(!preg_match("/^[a-zA-Z0-9]*$/", $username)){
-    $_SESSION['errors'][] = "Username should contain only alphanumeric characters". "</br>";
-    header('location: ./index.php');
-    exit();
-}
-else if(empty($lastname)){
-    $_SESSION['errors'][] = "Please Enter your last name". "</br>";
-    header('location: ./index.php');
-    exit();
-}
-else if(empty($firstname)){
-    $_SESSION['errors'][] = "Please Enter your first name". "</br>";
-    header('location: ./index.php');
-    exit();
-}
-else if(empty($username)){
-    $_SESSION['errors'][] = "Username is a required field". "</br>";
-    header('location: ./index.php');
-    exit();
-}
-else if(empty($password)){
-    $_SESSION['errors'][] = "Password is a required field". "</br>";
-    header('location: ./index.php');
-    exit();
-}
-else if(empty($confirmPassword)){
-    $_SESSION['errors'][] = "Password is a required field". "</br>";
-    header('location: ./index.php');
-    exit();
-}
-else if (!filter_var($email, FILTER_VALIDATE_EMAIL)){
-    $_SESSION['errors'][] = "Email is not in a valid format". "<br>";
-    header('location: ./index.php');
-exit();
-}
-else if ($password !== $confirmPassword){
-    $_SESSION['errors'][] = "Passwords do not match". "<br>";
-    header('location: ./index.php');
-exit();
-}
-else{
-//authenticating existence of username and email
-    
-$checkUser = "SELECT * FROM users WHERE username = '$username'";
-$checkemail = "SELECT * FROM users WHERE email = '$email'";
-$emresult = $conn->query($checkemail);
-$emailC = $emresult->fetch(PDO::FETCH_ASSOC);
 
-$result = $conn->query($checkUser);
-$user = $result->fetch(PDO::FETCH_ASSOC);
-if($user){
-    $_SESSION['errors'][] = "Username already exists. Please choose a different username";
-    header('location: ./index.php');
-    exit;}
-else if($emailC){
-    $_SESSION['errors'][] = "Inputed email already exists. Please choose a different email";
-    header('location: ./index.php');
-    exit; }
-else{
-    $passHash = password_hash($password, PASSWORD_DEFAULT);
-    $sql = "INSERT INTO users (firstname, lastname, username, email, password)
- VALUES ('$firstname', '$lastname', '$username', '$email', '$passHash')";
-$done = $conn->exec($sql);
-    // $_SESSION['success'] = "Sign up was successful, please use your registration details to login";
-    header('location: ../login.php?success=');
-    exit();
-} 
-   }
-}
-else{
-//user did not click submit but got here through url modification redirect back to login page
-header('location: ./index.php');
-exit();
-}
+include ('dbcon.php');
+if (isset($_POST['submit'])) {
+    $error = array();//Declare An Array to store any error message  
+    if (empty($_POST['fullname'])) {//if no name has been supplied 
+        $error[] = 'Please Enter a Full Name ';//add to array "error"
+    } else {
+        $fullname = $_POST['fullname'];//else assign it a variable
+		$fullname = trim($fullname); //removing white space from the user input 
+    }
+
+    if (empty($_POST['email'])) {
+        $error[] = 'Please Enter your Email ';
+    } else {
+
+
+        if (preg_match("^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$^", $_POST['email'])) {
+           //regular expression for email validation
+            $email = $_POST['email'];//assign it a variable
+			$email = trim($email);//removing white space from the user input 
+        } else {
+             $error[] = 'Your EMail Address is invalid  ';
+        }
+
+
+    }
+
+
+    if (empty($_POST['password'])) {
+        $error[] = 'Please Enter Your Password ';
+    } else {
+        $password1 = $_POST['password'];//assign it a variable
+		$password1 = trim($password1);//removing white space from the user input 
+    }
+	if (empty($_POST['confirm_password'])) {
+        $error[] = 'Please Enter confirmed Password ';
+    } else {
+        $confirm_password = $_POST['confirm_password'];//assign it a variable
+		$c_password = trim($c_password);//removing white space from the user input 
+    }
+	if ($password1 == $confirm_password ) {
+       $password = $_POST['password']; 
+    } else {
+		$error[] = 'Confirm password didnt matche ';
+    }
+	
+    if (empty($error)) //send to Database if there's no error '
+
+    { // If everything's OK...
+
+        // Make sure the email address is available:
+        $query_verify_email = "SELECT * FROM users  WHERE Email ='$email'";
+        $result_verify_email = mysqli_query($dbc, $query_verify_email);
+        if (!$result_verify_email) {//if the Query Failed ,similar to if($result_verify_email==false)
+            echo ' Database Error Occured ';
+        }
+
+        if (mysqli_num_rows($result_verify_email) == 0) { // IF no previous user is using this email .
+
+
+            // Create a unique  activation code:
+            //$activation = md5(uniqid(rand(), true));
+
+
+            $query_insert_user = "INSERT INTO `users` ( `Fullname`, `Email`, `Password` ) VALUES ( '$fullname', '$email', '$password')";
+
+
+            $result_insert_user = mysqli_query($dbc, $query_insert_user);
+            if (!$result_insert_user) {
+                echo 'Query Failed ';
+            }
+
+            if (mysqli_affected_rows($dbc) == 1) { //If the Insert Query was successfull.
+
+
+                // Send the email:
+                // $message = " To activate your account, please click on this link:\n\n";
+               //  $message .= WEBSITE_URL . '/activate.php?email=' . urlencode($Email) . "&key=$activation";
+                // mail($Email, 'Registration Confirmation', $message, 'From: ismaakeel@gmail.com');
+
+                // Flush the buffered output.
+
+
+                // Finish the page:
+                echo '<div class="success">Thank you '.$fullname.' For Sign Up with us </div>';
+
+
+				} 
+			else { // If it did not run OK.
+                echo '<div class="errormsgbox">You could not be registered due to a system error. We apologize for any inconvenience.</div>';
+            }
+
+        } 
+		else { // The email address is not available.
+            echo '<div class="errormsgbox" >That email address has already been registered. </div>';
+        }
+
+    } 
+	else {//If the "error" array contains error msg , display them
+        
+        
+
+echo '<div class="errormsgbox"> <ol>';
+        foreach ($error as $key => $values) {
+            
+            echo '	<li>'.$values.'</li>';
+
+
+       
+        }
+        echo '</ol></div>';
+
+    }
+  
+    mysqli_close($dbc);//Close the DB Connection
+
+} // End of the main Submit conditional.
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title> Budgit | Signup</title>
+    <link rel = "icon" href = "images\logo2.png" type = "image/png">
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css?family=Lobster|Lobster+Two&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+    <div class="container">
+        <div class="main">
+            <header>
+                <div id="logo-div">
+                    <img src="images/logo.png" id="logo" alt=""> <span id="logo-text">BudgIt</span> 
+                    <p>Making managing your finances hassle free</p>
+                </div>
+                <div id="nav-div" class="nav-div">
+                    <p> <a href="index.php" > Home</a>
+                        <a href=""> About</a>
+                        <a href="login.php" id="login"> Login</a>
+                        <a href="signup.php" id="signup" class="active"> Signup</a>
+                        <a href=""> Contact</a>
+                        <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+                                <img src="img/mdi_menu.png" alt="">
+                              </a>
+                    </p>
+                </div>
+            </header>
+            <div class="content">
+            
+                <h1>Signup </h1>
+                <p>It only takes a minute</p>
+
+                <form id="form" action="signup.php" method="POST">
+                    <input type="text" id="fullname" name="fullname"  placeholder="Full Name" required><span id="Evalid"></span><br><br>
+                    
+                    <input type="email" id="email" name="email"  placeholder="email" required><span id="Evalid"></span><br><br>
+                   
+                    <input type="password" name="password" id="password" placeholder="password" required style="width: 230px"><i class="fa fa-eye" id="view"></i><br><br>
+                   
+                    <input type="password" name="confirm_password" id="confirm_password" placeholder="Confirm password" required onkeyup='checkPassword();'>
+                    <span id="message"></span><br><br>
+                    
+                    <button id="submit" type="submit" name="submit"  >Sign Up</button><br>
+                    <span>By clicking the Sign Up button, you agree to our</span><br>
+                    <span><a href="">Terms & Conditions</a> and <a href=""> Privacy Policy</a></span>
+                </form>
+            
+            </div>
+            <div class="clear"></div>
+            <footer>
+                <b>&copy;Copyright 2019 Kymopoleia</b>
+            </footer>
+        </div>
+    </div>
+    <script src="script.js"></script>
+</body>
+</html>
